2e772624c0b9e8145a6f77a66f7cfad6cfdb47bd
1r-1na <irij666@gmail.com>
2023-11-03 00:06:35 +0100

    display instead of visibility

--- src/camera.js
+++ src/camera.js
@@ -1,8 +1,7 @@
 import backImage from "./x-circle.svg";
 import saveImage from "./save.svg";
-import stopImage from "./pause-btn.svg";
-import resumeImage from "./play-btn.svg";
-import cameraImage from "./camera.svg";
+import pauseImage from "./pause-btn.svg";
+import playImage from "./play-btn.svg";
 
 let width = 320; // We will scale the photo width to this
 let height = 0; // This will be computed based on the input stream
@@ -11,7 +10,10 @@ let streaming = false; //flag for a 1st-time init
 const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const photo = document.getElementById("photo");
-const cameraButton = document.getElementById("takephoto");
+const backButton = document.getElementById("back");
+const stopButton = document.getElementById("stop");
+const startButton = document.getElementById("start");
+const saveButton = document.getElementById("save");
 
 function adjustAspectRations(event) {
   //perform a one-time adjustment of video's and photo's aspect ratio
@@ -36,35 +38,51 @@ function takePicture() {
   const context = canvas.getContext("2d");
   context.drawImage(video, 0, 0, width, height);
 
-  imageData = canvas.toDataURL("image/jpeg");
+  const imageData = canvas.toDataURL("image/jpeg");
   photo.setAttribute("src", imageData);
-  video.setAttribute("visibility", "collapse");
-  photo.setAttribute("visibility", "visible");
 }
 
-cameraButton.addEventListener(
+stopButton.addEventListener(
   "click",
   (ev) => {
+    streaming = false;
     takePicture();
+    video.style.display = "none";
+    photo.style.display = "block";
+    stopButton.style.display = "none";
+    startButton.style.display = "block";
+    ev.preventDefault();
+  },
+  false
+);
+
+startButton.addEventListener(
+  "click",
+  (ev) => {
+    streaming = true;
+    video.style.display = "block";
+    photo.style.display = "none";
+    stopButton.style.display = "block";
+    startButton.style.display = "none";
     ev.preventDefault();
   },
   false
 );
 
 window.onload = () => {
-  const backButton = document.getElementById("back");
   backButton.src = backImage;
-  cameraButton.src = cameraImage;
-  const saveButton = document.getElementById("save");
+  stopButton.src = pauseImage;
+  startButton.src = playImage;
   saveButton.src = saveImage;
-  const stopButton = document.getElementById("stop");
-  stopButton.src = stopImage;
-  const resumeButton = document.getElementById("resume");
-  resumeButton.src = resumeImage;
-  photo.setAttribute("visibility", "collapse");
+
+  startButton.style.display = "none";
+  photo.style.display = "none";
 
   navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
+    .getUserMedia({
+      video: true,
+      audio: false,
+    })
     .then((stream) => {
       video.srcObject = stream;
       video.play();
